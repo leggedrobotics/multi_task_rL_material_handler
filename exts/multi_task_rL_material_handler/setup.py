c4cd358f9e7ff7fc0f44987a0b2458d2bc1d8e51
Nicolas Pfitzer <npfitzer@student.ethz.ch>
2024-10-08 17:12:35 +0200

    setup

--- exts/multi_task_rL_material_handler/setup.py
+++ exts/multi_task_rL_material_handler/setup.py
@@ -1,4 +1,4 @@
-"""Installation script for the 'ext_template' python package."""
+"""Installation script for the 'multi_task_rL_material_handler' python package."""
 
 import os
 import toml
@@ -18,8 +18,8 @@ INSTALL_REQUIRES = [
 
 # Installation operation
 setup(
-    name="ext_template",
-    packages=["ext_template"],
+    name="multi_task_rL_material_handler",
+    packages=["multi_task_rL_material_handler"],
     author=EXTENSION_TOML_DATA["package"]["author"],
     maintainer=EXTENSION_TOML_DATA["package"]["maintainer"],
     url=EXTENSION_TOML_DATA["package"]["repository"],
